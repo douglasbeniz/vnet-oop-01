6d617f6780ba76e1d43571a563e4cd579625b35d
Douglas Bezerra Beniz <douglas.bezerra.beniz@ess.eu>
2021-07-12 16:00:01 +0200

    Objects as arguments and parameters;

--- vnet_oop_01.py
+++ vnet_oop_01.py
@@ -4,6 +4,7 @@
 source: https://panda.ime.usp.br/pensepy/static/pensepy/13-Classes/classesintro.html
 """
 
+import math 
 
 class Point:
     """ Point class for representing and manipulating x,y coordinates. """
@@ -21,7 +22,31 @@ class Point:
     def distanceFromOrigin(self):
         return ((self.x ** 2) + (self.y ** 2)) ** 0.5
 
+    def __str__(self):
+        return "p(x:%s, y:%s)" % (self.x, self.y)
+
+
+def distance(point1, point2):
+    xdiff = point2.getX()-point1.getX()
+    ydiff = point2.getY()-point1.getY()
+
+    dist = math.sqrt(xdiff**2 + ydiff**2)
+    return dist
+
+# Object 1
+p = Point(4,3)
+q = Point(0,0)
+print("Distance from %s to origin, method 1: %f" % (str(p), distance(p,q)))
+
+p = Point(4,3)
+#print(p.distanceFromOrigin())
+print("Distance from %s to origin, method 1: %f" % (str(p), p.distanceFromOrigin()))
+
+# Object 2
 p = Point(7,6)
-print(p.distanceFromOrigin())
+q = Point(0,0)
+print("Distance from %s to origin, method 1: %f" % (str(p), distance(p,q)))
 
-
+p = Point(7,6)
+#print(p.distanceFromOrigin())
+print("Distance from %s to origin, method 1: %f" % (str(p), p.distanceFromOrigin()))
