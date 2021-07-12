d055f06ac88d92068c34fabd0e954a1e48ca6e13
Douglas Bezerra Beniz <douglas.bezerra.beniz@ess.eu>
2021-07-12 15:51:34 +0200

    Adding a method to calculate distance from origin;

--- vnet_oop_01.py
+++ vnet_oop_01.py
@@ -18,7 +18,10 @@ class Point:
     def getY(self):
         return self.y
 
+    def distanceFromOrigin(self):
+        return ((self.x ** 2) + (self.y ** 2)) ** 0.5
 
 p = Point(7,6)
-print(p.getX())
-print(p.getY())
+print(p.distanceFromOrigin())
+
+
