85ab38a18a61b52df98a4049b443338a502ef3e1
Douglas Bezerra Beniz <douglas.bezerra.beniz@ess.eu>
2021-07-12 15:45:12 +0200

    Adding other methods to our class;

--- vnet_oop_01.py
+++ vnet_oop_01.py
@@ -12,5 +12,13 @@ class Point:
 
         self.x = initX
         self.y = initY
+    def getX(self):
+        return self.x
+
+    def getY(self):
+        return self.y
+
 
 p = Point(7,6)
+print(p.getX())
+print(p.getY())
