7f03daaac9f3b4d204b5f3b8edff0f9df5c691fc
Douglas Bezerra Beniz <douglas.bezerra.beniz@ess.eu>
2021-07-12 15:01:19 +0200

    First point class version;

---
+++ vnet_oop_01.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""
+source: https://panda.ime.usp.br/pensepy/static/pensepy/13-Classes/classesintro.html
+"""
+
+
+class Point:
+    """ Point class for representing and manipulating x,y coordinates. """
+
+    def __init__(self):
+
+        self.x = 0
+        self.y = 0
+
+p = Point()         # Instantiate an object of type Point
+q = Point()         # and make a second point
+
+print("Nothing seems to have happened with the points")
+
