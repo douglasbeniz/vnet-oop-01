9b4181012d3559ca0e392c22632cc3da20f8a8b5
Douglas Bezerra Beniz <douglas.bezerra.beniz@ess.eu>
2021-07-12 15:33:51 +0200

    Verifying the instances;

--- vnet_oop_01.py
+++ vnet_oop_01.py
@@ -9,12 +9,15 @@ class Point:
     """ Point class for representing and manipulating x,y coordinates. """
 
     def __init__(self):
-
         self.x = 0
         self.y = 0
 
 p = Point()         # Instantiate an object of type Point
 q = Point()         # and make a second point
 
-print("Nothing seems to have happened with the points")
+print(Point)
+print(p)
+print(q)
+
+print(p is q)
 
